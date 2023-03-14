b5f03493fb86e339bc403c6b3d44862ac4711285
root <root@DESKTOP-ST3A33L.localdomain>
2023-03-14 21:41:39 +0900

    [Fix] builderror

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,16 @@
 "use client"
 
 import { Inter } from 'next/font/google'
-import { useRouter } from "next/navigation";
-import Cookies from "js-cookie";
-
+import { Codebox } from '@/components/CodeBox'
+import { Box } from '@mui/system'
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Top() {
-  //router
-  const router = useRouter();
-
-  //Cookieのチェック（これをいろいろ認証タイプにより変更）
-  const signedIn = Cookies.get("signedIn");
-  //signedInがtrueじゃなければ/loginへ
-  if (signedIn !== "true") router.replace("/login")
-  else {
-    router.replace("/main")
-  };
+export default function Main() {
+  return (
+    <main>
+        <Box>
+          <Codebox />
+        </Box>
+    </main>
+  )
 }
